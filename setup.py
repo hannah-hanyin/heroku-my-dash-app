c407707ac01a4e592bfd3906da2babe0f0739676
hanyin <hannah.hanyin@gmail.com>
2022-10-24 23:44:34 +1100

    add setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(     
+    name='my-nsw-business-map',
+    author='Han Yin',     
+    version='0.1',     
+    install_requires=[         
+        'pandas',         
+        'numpy',
+        'matplotlib',
+        'dash',
+        'dash_core_components',
+        'dash_bootstrap_components',
+        'dash_html_components',
+        'Plotly'
+    ],
+    # ... more options/metadata
+)
