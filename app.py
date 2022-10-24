e105e6ceb393f88f7b84bfa9e4310cf7621ed5f3
hanyin <hannah.hanyin@gmail.com>
2022-10-24 21:52:42 +1100

    app.py import update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dash import dcc
 from dash import html
 import plotly.graph_objs as go
 import pandas as pd
-from dash import dash_bootstrap_components as dbc
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 
 # Loading data
